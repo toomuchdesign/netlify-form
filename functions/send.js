3b6e6f484f46b9a51569cd41809ee9e1bcd47c42
Andrea Carraro <me@andreacarraro.it>
2020-05-09 23:43:21 +0200

    Provide lambda with payload

--- functions/send.js
+++ functions/send.js
@@ -1,13 +1,15 @@
 const sgMail = require('@sendgrid/mail');
 
 exports.handler = function(event, context, callback) {
+  const data = JSON.parse(event.body);
+  console.log('data', data);
+
   sgMail.setApiKey(process.env.SENDGRID_API_KEY);
   const msg = {
     to: 'me@andreacarraro.it',
-    from: 'hi@andreacarraro.it',
-    subject: 'Sending with Twilio SendGrid is Fun',
-    text: 'and easy to do anywhere, even with Node.js',
-    html: '<strong>and easy to do anywhere, even with Node.js</strong>',
+    from: data.from,
+    subject: 'Message from Netlify form',
+    text: data.message,
   };
   sgMail
     .send(msg)
--- src/index.js
+++ src/index.js
@@ -1,7 +1,22 @@
 var button = document.getElementById('submit');
+var input = document.getElementById('input');
+
 button.addEventListener('click', e => {
   e.preventDefault();
-  fetch('/.netlify/functions/send')
+  const value = input.value;
+  if (!value) {
+    return;
+  }
+
+  const data = {
+    from: 'hi@andreacarraro.it',
+    message: value,
+  };
+
+  fetch('/.netlify/functions/send', {
+    body: JSON.stringify(data),
+    method: 'POST',
+  })
     .then(() => {
       console.log('Message sent');
     })
