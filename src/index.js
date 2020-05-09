1b35a00038b7d719d82d36082ccddaaa10170af0
Andrea Carraro <me@andreacarraro.it>
2020-05-09 14:38:08 +0200

    Fix functions url

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ var button = document.getElementById('submit');
 button.addEventListener('click', e => {
   e.preventDefault();
   console.log('clicked');
-  fetch('/.netlify/lambda/send')
+  fetch('/.netlify/functions/send')
     .then(response => response.json())
     .then(data => console.log(data));
 });
