030a8ed2eb932ba59a2b9780beed50e19b3f5878
Andrea Carraro <me@andreacarraro.it>
2020-05-09 17:52:52 +0200

    Fix send grid API invication

--- functions/send-grid.js
+++ functions/send-grid.js
@@ -3,16 +3,16 @@ const sgMail = require('@sendgrid/mail');
 exports.handler = function(event, context, callback) {
   sgMail.setApiKey(process.env.SENDGRID_API_KEY);
   const msg = {
-    to: 'hi@andreacarraro.it',
-    from: 'test@example.com',
+    to: 'me@andreacarraro.it',
+    from: 'hi@andreacarraro.it',
     subject: 'Sending with Twilio SendGrid is Fun',
     text: 'and easy to do anywhere, even with Node.js',
     html: '<strong>and easy to do anywhere, even with Node.js</strong>',
   };
   sgMail
     .send(msg)
-    .then((response, body) => {
-      callback(null, {statusCode: response.statusCode, body});
+    .then(([res]) => {
+      callback(null, {statusCode: res.statusCode, body: res.body});
     })
     .catch(err => callback(err, null));
 };
