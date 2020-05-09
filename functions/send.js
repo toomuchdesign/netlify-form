0341c4020a05f7af0f1804e3bd0d31264554a32c
Andrea Carraro <me@andreacarraro.it>
2020-05-09 14:25:10 +0200

    Setup a lambda function

---
+++ functions/send.js
@@ -0,0 +1,22 @@
+const https = require('https');
+
+exports.handler = function(event, context, callback) {
+  const options = {
+    hostname: 'www.andreacarraro.it',
+    path:
+      '/scripts/contactmail.php?name=John&email=test%40gmail.com&test=Lorem&ajx=1',
+    method: 'GET',
+  };
+
+  const req = https.request(options, res => {
+    res.on('data', body => {
+      callback(null, {statusCode: res.statusCode, body});
+    });
+  });
+
+  req.on('error', error => {
+    callback(error);
+  });
+
+  req.end();
+};
--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
-console.log('hi!');
 var button = document.getElementById('submit');
 button.addEventListener('click', () => {
   console.log('clicked');
+  fetch('/.netlify/lambda/send')
+    .then(response => response.json())
+    .then(data => console.log(data));
 });
