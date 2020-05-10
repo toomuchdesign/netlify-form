e0500353cf49229e186898db45af4e6338f6906d
Andrea Carraro <me@andreacarraro.it>
2020-05-10 15:06:41 +0200

    Duplicate email sending logic

--- functions/send.js
+++ functions/send.js
@@ -1,4 +1,20 @@
 const sgMail = require('@sendgrid/mail');
+const SENDER_EMAIL = 'hi@andreacarraro.it';
+
+function sendEmail({to, subject, replyTo, text}) {
+  sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+  const msg = {
+    from: SENDER_EMAIL, // Validated address
+    to,
+    replyTo,
+    subject,
+    text,
+  };
+
+  return sgMail
+    .send(msg)
+    .then(([res]) => ({statusCode: res.statusCode, body: res.body}));
+}
 
 exports.handler = function(event, context, callback) {
   const data = JSON.parse(event.body);
@@ -6,20 +22,25 @@ exports.handler = function(event, context, callback) {
   // @TODO validate data
   if (data.spam) {
     callback(new Error('Validation error'));
+    return;
   }
 
-  sgMail.setApiKey(process.env.SENDGRID_API_KEY);
-  const msg = {
-    from: 'hi@andreacarraro.it', // Validated address
-    to: 'me@andreacarraro.it',
+  const emailUser = sendEmail({
+    to: data.from,
+    replyTo: SENDER_EMAIL,
+    subject: `Message received [TBD]`,
+    text: 'Message received. Thank you message [TBD]',
+  });
+
+  const emailOwner = sendEmail({
+    to: SENDER_EMAIL,
     replyTo: data.from,
-    subject: `Email from Netlify form from [${data.from}]`,
+    subject: `[project-name] message from ${data.from}`,
     text: data.message,
-  };
-  sgMail
-    .send(msg)
-    .then(([res]) => {
-      callback(null, {statusCode: res.statusCode, body: res.body});
-    })
-    .catch(err => callback(err, null));
+  });
+
+  // NOTE consider catching only emailUser error
+  Promise.all([emailUser, emailOwner])
+    .then(([emailUserResponse]) => callback(null, emailUserResponse))
+    .catch(errors => callback(errors, null));
 };
